89a265c9648f9f0bb1e2ff90bb48b93e891c94b4
adrian-trif-birdz <adrian.trif.ext@veolia.com>
2019-06-14 15:18:19 +0200

    Trading problem. Match trade

--- Dojo.Tests/DojoTests.fs
+++ Dojo.Tests/DojoTests.fs
@@ -5,7 +5,207 @@ open FsUnit
 open Dojo
 
 [<Test>]
-let ``first test``() =
-    add 1 2
-    |> should equal 3
+let ``given no order on the market match returns no match``() =
+    let trade =
+        {  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "EUR"
+            ToCurrency = "USD"
+            Ratio = 1.2m
+        }
+    matchTrades [] trade
+    |> should equal []
 
+[<Test>]
+let ``given one trade with a match the match should be returned``() =
+    let trade =
+        {  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "EUR"
+            ToCurrency = "USD"
+            Ratio = 1.2m
+        }
+    let markettrades =
+        [{  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "USD"
+            ToCurrency = "EUR"
+            Ratio = 0.84m
+        }]
+    matchTrades markettrades trade
+    |> should equal 
+        [{  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "USD"
+            ToCurrency = "EUR"
+            Ratio = 0.84m
+        }]
+
+[<Test>]
+let ``given some trades  with a match the match should be returned``() =
+    let trade =
+        {  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "EUR"
+            ToCurrency = "USD"
+            Ratio = 1.2m
+        }
+    let markettrades =
+        [
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.84m
+            }
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.80m
+            }
+        ]
+    matchTrades markettrades trade
+    |> should equal 
+        [{  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "USD"
+            ToCurrency = "EUR"
+            Ratio = 0.84m
+        }]
+
+[<Test>]
+let ``given some other trades  with a match the match should be returned``() =
+    let trade =
+        {  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "EUR"
+            ToCurrency = "USD"
+            Ratio = 1.2m
+        }
+    let markettrades =
+        [
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.80m
+            }
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.84m
+            }
+        ]
+    matchTrades markettrades trade
+    |> should equal 
+        [{  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "USD"
+            ToCurrency = "EUR"
+            Ratio = 0.84m
+        }]
+
+
+[<Test>]
+let ``given some trades  with NO match the matchTrades returns empty``() =
+    let trade =
+        {  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "EUR"
+            ToCurrency = "USD"
+            Ratio = 1.2m
+        }
+    let markettrades =
+        [
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.80m
+            }
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.83m
+            }
+        ]
+    matchTrades markettrades trade
+    |> should equal []
+
+[<Test>]
+let ``given trades in different currences with NO match the matchTrades returns empty``() =
+    let trade =
+        {  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "EUR"
+            ToCurrency = "USD"
+            Ratio = 1.2m
+        }
+    let markettrades =
+        [
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.80m
+            }
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "GBP"
+                Ratio = 0.88m
+            }
+        ]
+    matchTrades markettrades trade
+    |> should equal []
+
+[<Test>]
+let ``given trades in different currences with one match the matchTrades returns the match``() =
+    let trade =
+        {  
+            OrderId = "1"
+            Qty = 100
+            FromCurrency = "EUR"
+            ToCurrency = "USD"
+            Ratio = 1.2m
+        }
+    let markettrades =
+        [
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "EUR"
+                Ratio = 0.80m
+            }
+            {  
+                OrderId = "1"
+                Qty = 100
+                FromCurrency = "USD"
+                ToCurrency = "GBP"
+                Ratio = 0.88m
+            }
+        ]
+    matchTrades markettrades trade
+    |> should equal []
--- Dojo/Dojo.fs
+++ Dojo/Dojo.fs
@@ -1,5 +1,30 @@
 ﻿module Dojo
 
-let add left right =
-    left + right
+type Trade =
+    {
+        OrderId: string
+        Qty : int
+        FromCurrency: string
+        ToCurrency: string
+        Ratio: decimal
+    }
 
+
+let matchTrades (marketState: Trade list) tradeToMatch =
+    let currencyMatch t1 t2 =
+        t1.FromCurrency = t2.ToCurrency 
+        && t1.ToCurrency = t2.FromCurrency
+
+    match marketState with
+    | [] -> []
+    | _ ->
+        let result =
+            marketState
+            |> List.filter (currencyMatch tradeToMatch)
+            |> List.sortByDescending (fun t -> t.Ratio)
+            |> List.head
+        if (result.Ratio > 1m/tradeToMatch.Ratio)
+        then
+            [result]
+        else
+            []
