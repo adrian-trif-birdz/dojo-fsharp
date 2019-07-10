e774f98d18f4a194990cf8a9eb4ae8f9daa415c9
adrian-trif-birdz <adrian.trif.ext@veolia.com>
2019-07-10 14:44:55 +0200

    Trading Problem part 2

--- Dojo.Tests/DojoTests.fs
+++ Dojo.Tests/DojoTests.fs
@@ -5,7 +5,37 @@ open FsUnit
 open Dojo
 
 [<Test>]
-let ``first test``() =
-    add 1 2
-    |> should equal 3
+let ``Given no market state findTradingLoop returns no result``() =
+    findTradingLoop [] ""
+    |> should equal []
 
+[<Test>]
+let ``Given market state with no loops findTradingLoop returns no result``() =
+    let marketState = 
+        [
+            {Index=0; StartingQty=10m; StartingCurrency = "USD"; EndQty = 11m; EndingCurrency = "USD"}
+        ]
+    findTradingLoop marketState ""
+    |> should equal []
+
+[<Test>]
+let ``Given market state with a loop findTradingLoop returns a result``() =
+    let marketState = 
+        [
+            {Index=1; StartingQty=80m; StartingCurrency = "EUR"; EndQty = 100m; EndingCurrency = "USD"}
+            {Index=2; StartingQty=105m; StartingCurrency = "USD"; EndQty = 90m; EndingCurrency = "GBP"}
+            {Index=3; StartingQty=90m; StartingCurrency = "GBP"; EndQty = 80m; EndingCurrency = "EUR"}
+        ]
+    findTradingLoop marketState "USD"
+    |> should equal [1; 3; 2]
+
+[<Test>]
+let ``Given market state with a loop and a different starting currency findTradingLoop returns a different result``() =
+    let marketState = 
+        [
+            {Index=1; StartingQty=80m; StartingCurrency = "EUR"; EndQty = 100m; EndingCurrency = "USD"}
+            {Index=2; StartingQty=105m; StartingCurrency = "USD"; EndQty = 90m; EndingCurrency = "GBP"}
+            {Index=3; StartingQty=90m; StartingCurrency = "GBP"; EndQty = 80m; EndingCurrency = "EUR"}
+        ]
+    findTradingLoop marketState "GBP"
+    |> should equal [2; 1; 3]
--- Dojo/Dojo.fs
+++ Dojo/Dojo.fs
@@ -1,5 +1,31 @@
 ﻿module Dojo
 
-let add left right =
-    left + right
+type Trade = 
+    {
+        Index: int
+        StartingQty: decimal
+        StartingCurrency: string
+        EndQty: decimal
+        EndingCurrency: string
+    }
 
+let findTradingLoop marketState startCurrency =
+    let rec traverseMarket marketState currentCurrency stopCurrency acc =
+        let getFirstMatch marketState currency =
+            marketState
+            |> Seq.filter (fun trade -> trade.EndingCurrency = currency)
+            |> (fun x ->
+                if Seq.isEmpty x
+                then Option.None
+                else Some(Seq.head x))
+
+        let trade = getFirstMatch marketState currentCurrency
+        match trade with
+        | None -> []
+        | Some t -> if t.StartingCurrency = stopCurrency
+                    then List.append acc [t.Index] // to add the t.Index 
+                    else traverseMarket marketState t.StartingCurrency stopCurrency (List.append acc [t.Index])
+        
+    traverseMarket marketState startCurrency startCurrency []
+
+    
